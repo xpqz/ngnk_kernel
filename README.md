736c581dbd3aa22e680ece31db6b5559f4618cd5
Stefan kruger <stefan.kruger@gmail.com>
2021-11-18 10:09:08 +0000

    we have lift-off!

--- README.md
+++ README.md
@@ -1,19 +1,38 @@
-# A Jupyter kernel for bash
+# A Jupyter kernel for ngn/k
 
-This requires IPython 3.
+NOTE: This is alpha quality at best.
+
+This is a jupyter kernel for [ngn/k](https://codeberg.org/ngn/k), derived from the [bash_kernel](https://github.com/takluyver/bash_kernel).
+
+This kernel is designed to run in a docker container, which fetches the most bleeding edge of ngn/k and builds it from source. Note that ngn/k is a moving target.
+
+If you want to run this kernel 'natively' instead of in Docker, you need to do two things:
+
+1. Change the line
+
+    cmd = '/home/jovyan/k/k-libc /home/jovyan/k/repl.k'
+
+in `NgnkKernel` to reflect your ngn/k installation location.
+
+2. Change the prompt in `repl.k` and rebuild:
+
+    repl.prompt:"ngnk> "
+
+This is so that `pexpect` has something to look for. See the `Dockerfile`.
 
 To use the kernel, build and run the docker image:
 
     docker build -t ngnkern .
 
-Run container as `ktest`
+Run container as `ngnk`
 
-    docker run --name ktest --rm -p 8888:8888 -v ./notebooks ngnkern
+    docker run --name ngnk --rm -p 8888:8888 -v ./notebooks ngnkern
 
-To open a shell:
+If everything worked as intended, you should be given a URL to open.
 
-    docker exec -it ktest /bin/bash
+To open a shell on the running container, you can do
+
+    docker exec -it ngnk /bin/bash
 
 For details of how this works, see the Jupyter docs on [wrapper kernels](http://jupyter-client.readthedocs.org//en/latest/wrapperkernels.html), and
-Pexpect's docs on the [replwrap module](http://pexpect.readthedocs.org/en/latest/api/replwrap.html).
-
+`pexpect`'s docs on the [replwrap module](http://pexpect.readthedocs.org/en/latest/api/replwrap.html).
--- ngnk_kernel/kernel.py
+++ ngnk_kernel/kernel.py
@@ -1,3 +1,7 @@
+"""
+Derived from bash_kernel (https://github.com/takluyver/bash_kernel)
+"""
+
 from ipykernel.kernelbase import Kernel
 from pexpect import replwrap, EOF
 
@@ -9,15 +13,7 @@ version_pat = re.compile(r'version (\d+(\.\d+)+)')
 
 
 class IREPLWrapper(replwrap.REPLWrapper):
-    """A subclass of REPLWrapper that gives incremental output
-    specifically for bash_kernel.
-
-    The parameters are the same as for REPLWrapper, except for one
-    extra parameter:
-
-    :param line_output_callback: a callback method to receive each batch
-      of incremental output. It takes one string parameter.
-    """
+    # As parent, but with incremental output
     def __init__(self,
                  cmd_or_spawn,
                  orig_prompt,
@@ -32,26 +28,19 @@ class IREPLWrapper(replwrap.REPLWrapper):
                                       extra_init_cmd=extra_init_cmd)
 
     def _expect_prompt(self, timeout=-1):
-        if timeout == None:
-            # "None" means we are executing code from a Jupyter cell by way of the run_command
-            # in the do_execute() code below, so do incremental output.
+        if timeout is None:
             while True:
                 pos = self.child.expect_exact(
                     [self.prompt, self.continuation_prompt, u'\r\n'],
                     timeout=None)
                 if pos == 2:
-                    # End of line received
                     self.line_output_callback(self.child.before + '\n')
                 else:
                     if len(self.child.before) != 0:
-                        # prompt received, but partial line precedes it
                         self.line_output_callback(self.child.before)
                     break
         else:
-            # Otherwise, use existing non-incremental code
             pos = replwrap.REPLWrapper._expect_prompt(self, timeout=timeout)
-
-        # Prompt received, so return normally
         return pos
 
 
